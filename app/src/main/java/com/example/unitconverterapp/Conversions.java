ffb460336d3ef5dbd717bb33121cde726541b30c
Kaiden Larsen <kaidenlarsen555@gmail.com>
2023-09-15 03:40:03 -0400

    got the switch working

--- app/src/main/java/com/example/unitconverterapp/Conversions.java
+++ app/src/main/java/com/example/unitconverterapp/Conversions.java
@@ -1,32 +1,37 @@
 package com.example.unitconverterapp;
 
+import android.os.Message;
 import android.widget.Switch;
+import android.widget.Toast;
 
 public class Conversions {
-    public final static double DOLLAR_TO_EURO_MULTIPLIER = 0.939657;
-    public final static double EURO_TO_DOLLAR_MULTIPLIER = 1.06424;
-    public  final  static double MILE_TO_KILOMETER_MULTIPLIER = 1.609344;
-    public final static double KILOMETER_TO_MILE_MULTIPLIER = 0.6213712;
+
+    public final static double DOLLAR_TO_EURO_FINAL = 0.939657;
+    public final static double EURO_TO_DOLLAR_FINAL = 1.06424;
+    public final  static double MILE_TO_KILOMETER_FINAL = 1.609344;
+    public final static double KILOMETER_TO_MILE_FINAL = 0.6213712;
 
 
-    public static double dollarToEuro(double unit) {
-        double dollarConverted = unit * DOLLAR_TO_EURO_MULTIPLIER;
-        return dollarConverted;
-    }
 
     public static double euroToDollar(double unit) {
-        double euroConverted = unit * EURO_TO_DOLLAR_MULTIPLIER;
+        double euroConverted = unit * EURO_TO_DOLLAR_FINAL;
         return euroConverted;
     }
 
-    public static double mileToKilometer(double unit){
-        double milesConverted = unit * MILE_TO_KILOMETER_MULTIPLIER;
-        return milesConverted;
+    public static double dollarToEuro(double unit) {
+        double dollarConverted = unit * DOLLAR_TO_EURO_FINAL;
+        return dollarConverted;
     }
 
+
     public static double kilometerToMile(double unit){
-        double kilometersConverted = unit * KILOMETER_TO_MILE_MULTIPLIER;
+        double kilometersConverted = unit * KILOMETER_TO_MILE_FINAL;
         return kilometersConverted;
+    }
+
+    public static double mileToKilometer(double unit){
+        double milesConverted = unit * MILE_TO_KILOMETER_FINAL;
+        return milesConverted;
     }
 
 }
--- app/src/main/java/com/example/unitconverterapp/MainActivity.java
+++ app/src/main/java/com/example/unitconverterapp/MainActivity.java
@@ -2,34 +2,25 @@ package com.example.unitconverterapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Switch;
 import android.widget.TextView;
 
-/*
-
------------ TO DO LIST --------------
-input wont accept doubles, gotta figure that out
-
-gotta figure out how to use the switch
-
-make sure the app doesn't crash when you don't select a unit
-
-still suck at git and github
-
-is this even in MVC format? I think so?
- */
 
 public class MainActivity extends AppCompatActivity {
 
     private RadioGroup mConvertRadioGroup;
     private EditText mUnitsToConvert;
     private TextView mOutputTextview;
-    private Switch mUnitSwitch;
+    private RadioButton mEurosToDollarsRadio;
+    private RadioButton mKilometersToMilesRadio;
+    boolean switchOn = false;
 
 
     @Override
@@ -39,10 +30,30 @@ public class MainActivity extends AppCompatActivity {
         mConvertRadioGroup = findViewById(R.id.convert_radio_group);
         mUnitsToConvert = findViewById(R.id.units_to_convert);
         mOutputTextview = findViewById(R.id.output_textview);
-        mUnitSwitch = findViewById(R.id.unit_switch);
+        @SuppressLint("UseSwitchCompatOrMaterialCode") Switch mUnitSwitch = findViewById(R.id.unit_switch);
+        mKilometersToMilesRadio = findViewById(R.id.kilometers_to_miles_radio);
+        mEurosToDollarsRadio = findViewById(R.id.euros_to_dollars_radio);
+
+        //This is what monitors the switch, whenever the switch is toggled the switchOn bool is toggled here.
+        mUnitSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+
+                if(isChecked) {
+                    switchOn = true;
+                    mEurosToDollarsRadio.setText("Euros to Dollars");
+                    mKilometersToMilesRadio.setText("Kilometers to Miles");
+                } else {
+                    switchOn = false;
+                    mEurosToDollarsRadio.setText("Dollars to Euros");
+                    mKilometersToMilesRadio.setText("Miles to Kilometers");
+                }
+            }
+        });
     }
 
     public void convertClick(View view) {
+
         //copying most of the try catch from pizza party. IDK if it will work but we will see...
         double mainUnit;
         try {
@@ -54,20 +65,23 @@ public class MainActivity extends AppCompatActivity {
         }
         //honestly... I barely know whats going on here with the try catch, just happy it works.
 
-        //get either currency or distance selection from radio button group
-        int checkedRadioButton = mConvertRadioGroup.getCheckedRadioButtonId();
-        if (checkedRadioButton == R.id.euros_to_dollars_radio) {
+        /*get either currency or distance selection from radio button group also checks if the switch is on and makes this outputText the calculation
+         using the appropriate method from Conversions.java */
 
-            //The below code simply sets the output box to the calculated number from the euroToDollar() method in Conversions.java
+        int checkedRadioButton = mConvertRadioGroup.getCheckedRadioButtonId();
+
+        if(checkedRadioButton == R.id.euros_to_dollars_radio && switchOn) {
+            String outputText = String.valueOf(Conversions.dollarToEuro(mainUnit));
+            mOutputTextview.setText(outputText);
+        } else if(checkedRadioButton == R.id.euros_to_dollars_radio) {
             String outputText = String.valueOf(Conversions.euroToDollar(mainUnit));
             mOutputTextview.setText(outputText);
+        } else if (checkedRadioButton == R.id.kilometers_to_miles_radio && switchOn) {
+            String outputText = String.valueOf(Conversions.mileToKilometer(mainUnit));
+            mOutputTextview.setText(outputText);
         } else if (checkedRadioButton == R.id.kilometers_to_miles_radio) {
-
-            //Same thing as the code above, basically copy pasted
             String outputText = String.valueOf(Conversions.kilometerToMile(mainUnit));
             mOutputTextview.setText(outputText);
         }
-
-
     }
 }
